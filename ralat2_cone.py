077f46bc379302dbcba4f7774c98a5ff71c353f9
Yvonne-Ting-Shi-Yun <163949881+Yvonne-Ting-Shi-Yun@users.noreply.github.com>
2024-09-24 08:46:22 +0800

    Update ralat2_cone.py 2

--- ralat2_cone.py
+++ ralat2_cone.py
@@ -7,15 +7,15 @@ pi = 3.142
 def dapat_jejari_tinggi():
     a = float(input("Masukkan jejari: "))
     b = float(input("Masukkan tinggi: "))
-    return (d, c)
+    return (a, b)
 
 def kira_luas_permukaan_kon(r, h):
-    luas_permukaan_kon = (pi * h) * (r + maths.squareroot(r**3 + h**3))
-    return round(luass_permukaan_kon, 2)
+    luas_permukaan_kon = (pi * r） * (r + math.sqrt(r**2 + h**2))
+    return round(luas_permukaan_kon, 2)
 
 def main_cone():
-    (x, y) = dapat_jejari_tinngi()
-    luas_permukaan_kon = kira_luas_permukaann_kon(x, z)
+    (x, y) = dapat_jejari_tinggi()
+    luas_permukaan_kon = kira_luas_permukaan_kon(x, y)
     print(f"luas permukaan kon = {luas_permukaan_kon:.2f}")
 
 # JANGAN ubah kod di bawah baris ini!
