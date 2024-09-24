9aef02560a8308c5b13803683c23bc60c8c0fb54
Yvonne-Ting-Shi-Yun <163949881+Yvonne-Ting-Shi-Yun@users.noreply.github.com>
2024-09-24 08:50:48 +0800

    Update ralat2_cone.py 2

--- ralat2_cone.py
+++ ralat2_cone.py
@@ -10,7 +10,7 @@ def dapat_jejari_tinggi():
     return (a, b)
 
 def kira_luas_permukaan_kon(r, h):
-    luas_permukaan_kon = (pi * r） * (r + math.sqrt(r**2 + h**2))
+    luas_permukaan_kon = (pi * r) * (r + math.sqrt(r**2+ h**2))
     return round(luas_permukaan_kon, 2)
 
 def main_cone():
